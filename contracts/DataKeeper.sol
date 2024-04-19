0f11f2cd3bc8f6f98182c31046dff5a2df1d09b8
Stepan <stkolmagorov@gmail.com>
2024-04-19 15:02:26 +0700

    datakeeper added

--- contracts/DataKeeper.sol
+++ contracts/DataKeeper.sol
@@ -2,18 +2,33 @@
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 contract DataKeeper is Ownable {
-    bytes32 public merkleRoot;
+    using EnumerableSet for EnumerableSet.Bytes32Set;
 
-    event MerkleRootUpdated(bytes32 indexed oldMerkleRoot, bytes32 indexed newMerkleRoot);
+    EnumerableSet.Bytes32Set private _merkleRoots;
 
-    function setMerkleRoot(bytes32 merkleRoot_) external onlyOwner {
-        emit MerkleRootUpdated(merkleRoot, merkleRoot_);
-        merkleRoot = merkleRoot_;
+    event MerkleRootAdded(bytes32 indexed merkleRoot);
+
+    function addMerkleRoot(bytes32 merkleRoot_) external onlyOwner {
+        _merkleRoots.add(merkleRoot_);
+        emit MerkleRootAdded(merkleRoot_);
     }
 
-    function verify(bytes32 leaf_, bytes32[] calldata merkleProof_) external view returns (bool isValid_) {
+    function numberOfRoots() external view returns (uint256) {
+        return _merkleRoots.length();
+    }
+
+    function getMerkleRootAt(uint256 index_) external view returns (bytes32) {
+        return _merkleRoots.at(index_);
+    }
+
+    function isMerkleRootInArray(bytes32 merkleRoot_) external view returns (bool) {
+        return _merkleRoots.contains(merkleRoot_);
+    }
+
+    function verify(bytes32 merkleRoot_, bytes32 leaf_, bytes32[] calldata merkleProof_) external pure returns (bool isValid_) {
         assembly {
             let ptr := merkleProof_.offset
             for { let end := add(ptr, mul(0x20, merkleProof_.length)) } lt(ptr, end) { ptr := add(ptr, 0x20) } {
@@ -29,7 +44,7 @@ contract DataKeeper is Ownable {
                 }
                 leaf_ := keccak256(0x00, 0x40)
             }
-            isValid_ := eq(sload(merkleRoot.slot), leaf_)
+            isValid_ := eq(merkleRoot_, leaf_)
         }
     }
 }
--- scripts/DataKeeperDeployment.js
+++ scripts/DataKeeperDeployment.js
@@ -5,6 +5,7 @@ async function main () {
     const dataKeeper = await DataKeeper.deploy();
     await dataKeeper.waitForDeployment();
     console.log("Deployed to:", dataKeeper.target);
+    await dataKeeper.transferOwnership("0xfdC788a46beCa9fCC6Ba46f2151ea53Be3477f11");
 }
 
 main()
